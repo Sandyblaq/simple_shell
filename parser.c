bbedc85707bb53fa5118494ffa346059409d8480
Sandyblaq <boyleruth764@gmail.com>
2023-06-23 08:33:52 -0700

    parser.c

---
+++ parser.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * is_cmd - will determine if a file is an executable cmd
+ * @info: at the info struct
+ * @path: the path to the file
+ *
+ * Return: 1 if success, 0 otherwise
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - matches characters
+ * @pathstr: at the PATH str
+ * @start: now starts the index
+ * @stop: now stops the index
+ *
+ * Return: points to the new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - find the command in the PATH str
+ * @info: at the info struct
+ * @pathstr: at the PATH str
+ * @cmd: the command to find
+ *
+ * Return: the full path of the command if found or void
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
