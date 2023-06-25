8b717a3f81a6ca6c2f953ab2a8937eaf5e0a7fb6
Sandyblaq <boyleruth764@gmail.com>
2023-06-25 11:17:24 -0700

    _atoi.c

---
+++ _atoi.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ * interactive - outputs True if shell is on interactive mode
+ * @info: print struct address
+ *
+ * Return: 1 on interactive mode, or otherwise 0
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - tests if the character is a delimiter
+ * @c: the character to test
+ * @delim: delimiter string
+ * Return: 1 if success, 0 if failure
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - tests for alphabetic character
+ *@c: the char to input
+ *Return: 1 if the c is alphabetic, or otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - change the string to an integer
+ *@s: the string to be changed
+ *Return: 0 if the string has no numbers, or otherwise the changed number
+ */
+
+int _atoi(char *s)
+{
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
