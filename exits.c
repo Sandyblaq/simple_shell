97aa928a0862c95e4f3adfac1465b017cbd2c3dc
Sandyblaq <boyleruth764@gmail.com>
2023-06-25 17:44:02 -0700

    exits.c

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - duplicates a string
+ *@dest: the destination string to be duplicated to
+ *@src: source string
+ *@n: the sum of characters to be duplicated
+ *Return: the linked string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - links two strings
+ *@dest: first string
+ *@src: second string
+ *@n: the sum of bytes to be maximally used
+ *Return: the linked string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - finds a character in a string
+ *@s: string to be tokenized
+ *@c: the character to be located
+ *Return: (s) points to the memory area s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
